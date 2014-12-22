354266aad6475782b99a252c2b6a16943a78a794
Rajshekar <rajshekarsv3@gmail.com>
2014-12-22 22:27:09 +0530

    changed fb secret key

--- server/accounts.js
+++ server/accounts.js
@@ -5,7 +5,7 @@ ServiceConfiguration.configurations.insert({
   service: "facebook",
   appId: "1534395086796051",
   loginStyle: "redirect",
-  secret: "4f5099337af28b6292c263677a5d4c97"
+  secret: "f47af10c39127278d2a866aa4ad13bed"
 });
 ServiceConfiguration.configurations.remove({
   service: "google"
